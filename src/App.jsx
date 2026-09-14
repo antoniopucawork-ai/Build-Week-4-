723daa4e79318303dfef954b0a7ed0c5da1c0b4d
Antonio-Puca <antoniopuca.work@gmail.com>
2026-09-14 20:08:55 +0200

    Setup struttura iniziale componenti

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { User, Home, Briefcase } from "lucide-react";
+
+function App() {
+  return (
+    <div>
+      <Home size={24} />
+      <User size={24} />
+      <Briefcase size={24} />
+    </div>
+  );
+}
+
+export default App;
