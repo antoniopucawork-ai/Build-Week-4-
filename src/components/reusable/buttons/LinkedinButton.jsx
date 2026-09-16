3ae8d9b3aa94bfba81d8783b9e43a4d2f3bab4bf
serenap06 <168187800+serenap06@users.noreply.github.com>
2026-09-17 01:50:59 +0200

    added customized reusable Buttons

---
+++ src/components/reusable/buttons/LinkedinButton.jsx
@@ -0,0 +1,138 @@
+import "./css/LinkedinButton.css";
+import {
+  UserPlus,
+  Plus,
+  Check,
+  Pencil,
+  X,
+  SquareArrowOutUpRight,
+} from "lucide-react";
+
+export const BUTTON_VARIANT = {
+  MAIN: {
+    AVAILABLE: {
+      label: "Disponibile per",
+      icon: null,
+      variant: "primary",
+    },
+    ADD_SECTION: {
+      label: "Aggiungi sezione",
+      icon: null,
+      variant: "outline",
+    },
+    EHNANCE: {
+      label: "Migliora profilo",
+      icon: null,
+      variant: "outline",
+    },
+    RESOURCE: {
+      label: "Risorse",
+      icon: null,
+      variant: "outline-grey",
+    },
+    CREATE: {
+      label: "Crea un post",
+      icon: null,
+      variant: "outline",
+    },
+    ADD_APP: {
+      label: "Aggiungi app collegate",
+      icon: null,
+      variant: "outline",
+    },
+    CREDENTIAL: {
+      label: "Mostra credenziale",
+      icon: SquareArrowOutUpRight,
+      variant: "outline-grey",
+    },
+    FOLLOWED: {
+      label: "Già segui",
+      icon: Check,
+      variant: "outline-grey",
+    },
+  },
+  ASIDE: {
+    TRY: {
+      label: "Prova gratis",
+      icon: null,
+      variant: "outline",
+    },
+    VIEW: {
+      label: "Visualizza",
+      icon: null,
+      variant: "outline-grey",
+    },
+    CONNECT: {
+      label: "Collegati",
+      icon: UserPlus,
+      variant: "outline-grey",
+    },
+    FOLLOW: {
+      label: "Segui",
+      icon: Plus,
+      variant: "outline-grey",
+    },
+  },
+  MODAL: {
+    SAVE: {
+      label: "Salva",
+      icon: null,
+      variant: "primary",
+    },
+    ADD_SKILL: {
+      label: "Aggiungi competenza",
+      icon: Plus,
+      variant: "outline",
+    },
+    ADD_MEDIA_CONTENT: {
+      label: "Aggiungi contenuto multimediale",
+      icon: Plus,
+      variant: "outline",
+    },
+  },
+  ICON_ONLY: {
+    PLUS: {
+      label: null,
+      icon: Plus,
+      variant: "icon-only",
+    },
+    EDIT: {
+      label: null,
+      icon: Pencil,
+      variant: "icon-only",
+    },
+    DELETE: {
+      label: null,
+      icon: X,
+      variant: "icon-only-outline",
+    },
+    COVER_IMAGE: {
+      label: null,
+      icon: Pencil,
+      variant: "icon-only-secondary"
+    }
+  },
+};
+
+const LinkedinButton = ({ customVariant, variant, iconOnly = false, onClick, disabled = false, size, className = "", ...props }) => {
+  const { icon: Icon, label: labelText, variant: configVariant } = customVariant || {};
+
+  const activeVariant = variant || configVariant || "primary"
+  const isIconOnly = iconOnly || !labelText;
+
+  return (
+    <>
+      <button
+        className={`btn-linkedin btn-${activeVariant} btn-${size} ${isIconOnly ? "btn-icon-only-base" : ""} ${className}`}
+        onClick={onClick}
+        disabled={disabled}
+        {...props}
+      >
+        {Icon && <Icon className="btn-icon" size={18} />}
+        {!isIconOnly && labelText && <span className="btn-label">{labelText}</span>}
+      </button>
+    </>
+  );
+};
+
+export default LinkedinButton;
