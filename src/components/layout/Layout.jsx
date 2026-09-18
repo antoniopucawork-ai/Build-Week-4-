ab8736c6744f7c7fa4f23f755f1c07b02c179851
devHP-source <delposogideonpaulo@yahoo.it>
2026-09-18 18:46:40 +0200

    added Footer

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,17 +1,19 @@
 import { Outlet, useLocation } from "react-router-dom"
 import Navigation from "./navigation/Navigation"
+import Footer from "./footer/Footer"
 
 const Layout = () => {
   const { pathname } = useLocation()
   const section = pathname.split("/")[1]
 
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <Navigation />
-      <main key={section} className="page-enter">
+      <main key={section} className="page-enter flex-grow-1">
         <Outlet />
       </main>
-    </>
+      <Footer />
+    </div>
   )
 }
 
