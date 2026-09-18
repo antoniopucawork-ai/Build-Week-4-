ffc49ed7ba2aa509987308f5f49b2371ceb9b196
Antonio-Puca <antoniopuca.work@gmail.com>
2026-09-18 18:30:45 +0200

    add information section

--- src/components/main/MainLayout.jsx
+++ src/components/main/MainLayout.jsx
@@ -1,15 +1,16 @@
 import HeroCard from "./mainsection/herocard/HeroCard";
 import RecommendedForYou from "./mainsection/recommendedForYou/RecommendedForYou";
 import Analysis from "./mainsection/analysis/Analysis";
-
+import Information from "./mainsection/information/Information";
 const Main = () => {
   return (
     <main className="d-flex flex-column gap-2">
       <HeroCard />
 
       <RecommendedForYou />
-      
+
       <Analysis />
+      <Information />
     </main>
   );
 };
