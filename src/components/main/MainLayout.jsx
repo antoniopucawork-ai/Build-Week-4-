646441a3e4f02bedec5cfffa82642e32a37f0770
serenap06 <168187800+serenap06@users.noreply.github.com>
2026-09-19 17:28:33 +0200

    added main generic cards

--- src/components/main/MainLayout.jsx
+++ src/components/main/MainLayout.jsx
@@ -3,6 +3,7 @@ import RecommendedForYou from "./mainsection/recommendedForYou/RecommendedForYou
 import Analysis from "./mainsection/analysis/Analysis";
 import Information from "./mainsection/information/Information";
 import MainCarouselCard from "./mainsection/mainCarouselCard/MainCarouselCard";
+import MainGenericCard from "./mainsection/MainGenericCard/MainGenericCard";
 
 const Main = () => {
   return (
@@ -16,6 +17,12 @@ const Main = () => {
       <Information />
 
       <MainCarouselCard />
+
+      <MainGenericCard title="Esperienza" />
+      <MainGenericCard title="Formazione" />
+      <MainGenericCard title="Licenze e certificazioni" />
+      <MainGenericCard title="Progetti" />
+      <MainGenericCard title="Lingue" />
     </section>
   );
 };
